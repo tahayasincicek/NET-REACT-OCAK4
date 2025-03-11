7a1a541abadb8421d2c0b5f3c8c332cd0a47c537
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-11 23:03:46 +0300

    Add files via upload

---
+++ Sum of numbers/Sum of numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        
+        int sum = 0;
+
+        
+        for (int i = 1; i <= number; i++)
+        {
+            sum += i;  
+        }
+
+        
+        Console.WriteLine("The sum of numbers from 1 to " + number + " is: " + sum);
+    }
+}
